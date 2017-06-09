fd369f52cedbd550edae1a00887eb2554f315a63
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2017-06-09 16:38:09 +0200

    use StringDistances

--- src/DocSeeker.jl
+++ src/DocSeeker.jl
@@ -3,17 +3,12 @@ module DocSeeker
 import Documenter: Utilities, DocSystem
 import Base.Iterators: flatten
 import Base.Docs: levenshtein
+using StringDistances
 
 # TODO: Be a bit more intelligent about this.
 function score(needle::String, s::String)
-  words = split(s, ['\n', ' '])
-  score = 0.0
-  for w in words
-    d = levenshtein(needle, w)
-    d > 5 && continue
-    score += 1/d
-  end
-  score
+  #TODO: Normalization of inputs.
+  compare(TokenMax(Jaro()), lowercase(needle), lowercase(s))
 end
 
 score(needle::String, s::Markdown.MD) = score(needle, Markdown.plain(s))
@@ -41,7 +36,7 @@ function searchdocs(needle::String, mod::Module=Main)
   docs = collect(flatten(alldocs()))
   scores = score.(needle, docs)
   perm = sortperm(scores, rev=true)[1:20]
-  [x.data[:binding] for x in docs[perm]]
+  scores[perm], docs[perm]
 end
 
 end # module
