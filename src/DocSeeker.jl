d81e1ebb774dcd242550eaceae3c869e9f9c243c
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2017-06-24 14:03:47 +0200

    penalize non-exported bindings
    and handle undocumented names correctly

--- src/DocSeeker.jl
+++ src/DocSeeker.jl
@@ -6,14 +6,16 @@ using StringDistances
 using Juno, Hiccup
 
 # TODO: figure out how to get something useable out of `DocObj.sig`
+# TODO: figure out how to save `sig` and not kill serialization
 struct DocObj
   name::String
   mod::String
   typ::String
-  sig::Any
+  # sig::Any
   text::String
   path::String
   line::Int
+  exported::Bool
 end
 
 # TODO: better string preprocessing.
@@ -30,12 +32,22 @@ function score(needle::String, s::Docs.DocStr)
 end
 
 function score(needle::String, s::DocObj)
+  score = 0.0
+  length(needle) == 0 && return score
+
   binding = s.name
-  length(s.text) == 0 && return compare(Hamming(), needle, binding)
-  doc = lowercase(Docs.stripmd(Markdown.parse(s.text)))
-  # max(compare(Jaro(), needle, binding), 0.8*compare(TokenSet(Jaro()), lowercase(needle), doc))
-  0.75*compare(Jaro(), needle, binding) + 0.25*compare(TokenSet(Jaro()), lowercase(needle), doc)
-  # 0.6*fuzzaldrin_score(needle, binding) + 0.4*compare(TokenSet(Jaro()), lowercase(needle), doc)
+  # TODO: figure out whyt this sometimes throws a `MethodError: no method matching stripmd(::Symbol)`
+  # doc = lowercase(Docs.stripmd(Markdown.parse(s.text)))
+  doc = lowercase(s.text)
+
+  # penalty if binding has no docs
+  score += (length(s.text) == 0 ? 0.74 : 0.75)*compare(Jaro(), needle, binding)
+  score += 0.25*compare(TokenSet(Jaro()), lowercase(needle), doc)
+
+  # penalty if binding isn't exported
+  score *= s.exported ? 1.0 : 0.99
+
+  return score
 end
 
 # rendering methods
--- src/introspective.jl
+++ src/introspective.jl
@@ -1,25 +1,31 @@
 
 maindoccache = DocObj[]
-maincachelastupdated = time()
+maincachelastupdated = 0
 
+function Base.search(needle::String; loaded = true, mod::Module = Main)
+  if loaded
+    dynamicsearch(needle, mod)
+  else
+    dynamicsearch(needle, mod, loaddocsdb())
+  end
+end
 
-function dynamicsearch(needle::String, mod::Module = Main)
-  docs = collect(alldocs(mod))
+function dynamicsearch(needle::String, mod::Module = Main, docs = alldocs())
   isempty(docs) && return ([], [])
   scores = score.(needle, docs)
   perm = sortperm(scores, rev=true)[1:min(20, length(docs))]
   scores[perm], docs[perm]
 end
 
-function modulebindings(mod, binds = Dict{Module, Vector{Symbol}}(), seenmods = Set{Module}())
-  for name in names(mod, true)
+function modulebindings(mod, exported = false, binds = Dict{Module, Vector{Symbol}}(), seenmods = Set{Module}())
+  for name in names(mod, !exported, false)
     if isdefined(mod, name) && !Base.isdeprecated(mod, name)
       obj = getfield(mod, name)
       !haskey(binds, mod) && (binds[mod] = [])
       push!(binds[mod], name)
       if (obj isa Module) && !(obj in seenmods)
         push!(seenmods, obj)
-        modulebindings(obj, binds, seenmods)
+        modulebindings(obj, exported, binds, seenmods)
       end
     end
   end
@@ -38,28 +44,57 @@ function alldocs(mod = Main)
   mod == Main && time() - maincachelastupdated < 1e4 && return maindoccache
 
   results = DocObj[]
-  modbinds = modulebindings(mod)
+  # all bindings
+  modbinds = modulebindings(mod, false)
+  # exported bindings only
+  exported = modulebindings(mod, true)
   for mod in keys(modbinds)
     meta = Docs.meta(mod)
-    for (binding, multidoc) in meta
-      for sig in multidoc.order
-        d = multidoc.docs[sig]
-        var = binding.var
-        mod = binding.mod
-        dobj = DocObj(string(var), string(mod), string(determinetype(mod, var)),
-                      sig, join(d.text, ' '), d.data[:path], d.data[:linenumber])
-        push!(results, dobj)
+    metanames = Set(collect(keys(meta)))
+    for name in modbinds[mod]
+      b = Docs.Binding(mod, name)
+      expb = haskey(exported, mod) && name in exported[mod]
+
+      if b in metanames
+        multidoc = meta[b]
+        for sig in multidoc.order
+          d = multidoc.docs[sig]
+          var = b.var
+          mod = b.mod
+          dobj = DocObj(string(var), string(mod), string(determinetype(mod, var)),
+                        # sig,
+                        join(d.text, ' '), d.data[:path], d.data[:linenumber], expb)
+          push!(results, dobj)
+        end
+      elseif !startswith(string(name), '#') && isdefined(mod, name) && !Base.isdeprecated(mod, name) && name != :Vararg
+        bind = getfield(mod, name)
+        meths = methods(bind)
+        if !isempty(meths)
+          for m in meths
+            dobj = DocObj(string(name), string(mod), string(determinetype(mod, name)),
+                          "", m.file, m.line, expb)
+            push!(results, dobj)
+          end
+        else
+          dobj = DocObj(string(name), string(mod), string(determinetype(mod, name)),
+                        "", "", 0, expb)
+          push!(results, dobj)
+        end
       end
     end
   end
+  # update cache
   if mod == Main
     maincachelastupdated = time()
     maindoccache = results
   end
-  results
+
+  return results
 end
 
 function determinetype(mod, var)
+  (isdefined(mod, var) && !Base.isdeprecated(mod, var)) || return ""
+
   b = getfield(mod, var)
 
   b isa Function && return "Function"
