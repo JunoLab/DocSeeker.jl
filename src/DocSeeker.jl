a6b5c9cda4ea4c592258f88c3471ecbf3517343d
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2017-07-02 21:21:36 +0200

    restructure, delete unneeded methods

--- src/DocSeeker.jl
+++ src/DocSeeker.jl
@@ -20,41 +20,31 @@ end
 
 # TODO: better string preprocessing.
 """
-    score(needle, s::Docs.DocStr) -> Float
+    score(needle, s::Docs.DocObj) -> Float
 
 Scores `s` against the search query `needle`. Returns a `Float` between 0 and 1.
 """
-function score(needle::String, s::Docs.DocStr)
-  binding = haskey(s.data, :binding) ? string(s.data[:binding].var) : ""
-  length(s.text) == 0 && return compare(Hamming(), needle, binding)
-  doc = lowercase(Docs.stripmd(get(s.object, Markdown.parse(join(s.text, ' ')))))
-  max(compare(Jaro(), needle, binding), 0.8*compare(TokenSet(Jaro()), lowercase(needle), doc))
-end
-
 function score(needle::String, s::DocObj)
   score = 0.0
   length(needle) == 0 && return score
 
   binding = s.name
-  # TODO: figure out whyt this sometimes throws a `MethodError: no method matching stripmd(::Symbol)`
+  # TODO: figure out why this sometimes throws a `MethodError: no method matching stripmd(::Symbol)`
   # doc = lowercase(Docs.stripmd(Markdown.parse(s.text)))
   doc = lowercase(s.text)
 
-  # penalty if binding has no docs
-  score += (length(s.text) == 0 ? 0.74 : 0.75)*compare(Winkler(Jaro()), needle, binding)
+  score += 0.75*compare(Winkler(Jaro()), needle, binding)
   score += 0.25*compare(TokenSet(Jaro()), lowercase(needle), doc)
 
+  # penalty if binding has no docs
+  length(s.text) == 0 && (score *= 0.99)
   # penalty if binding isn't exported
-  score *= s.exported ? 1.0 : 0.99
+  s.exported || (score *= 0.99)
 
   return score
 end
 
-# rendering methods
-function Juno.render(i::Juno.Inline, d::Docs.DocStr)
-  Juno.render(i, Juno.Tree(d.data[:binding], [Markdown.parse(join(d.text, ' '))]))
-end
-
+# rendering method
 function Juno.render(i::Juno.Inline, d::DocObj)
   Juno.render(i, Juno.Tree(span(span(".syntax--support.syntax--function", d.name), span(" @ $(d.path):$(d.line)")), [Markdown.parse(d.text)]))
 end
